46617bea6953f36c23c4ecd1f9d2a5973e55879e
TaherKD <taher.dhilawala@yahoo.com>
2016-12-04 15:21:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //change content of main-text div
 var element = document.getElementById('main-text');
-element.innerHTML = 'Hello World';
+element.innerHTML = 'Hello World!';
 
 //Move the image
 var img = document.getElementById('dragoon');
