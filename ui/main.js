7df1012249126f85656d158ce60450a20b1cf62c
TaherKD <taher.dhilawala@yahoo.com>
2016-12-04 20:00:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello World!';
 
 //Move the image
-var img = document.getElementById('dra');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft = '160px';
 };
