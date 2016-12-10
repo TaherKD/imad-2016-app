e59978d26049ba2a3b66ad2bd5b76b4976fc16ab
TaherKD <taher.dhilawala@yahoo.com>
2016-12-10 16:21:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,8 +42,6 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn'); 
 submit.onclick = function(){
     
@@ -70,6 +68,8 @@ submit.onclick = function(){
     };
     
     //make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://taherkd.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 }
